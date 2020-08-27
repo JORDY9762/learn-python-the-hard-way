160f435df1d67b34f6bb0a3fce660e3cb8651865
JORDY9762 <68658177+JORDY9762@users.noreply.github.com>
2020-08-27 23:40:53 +0100

    Add files via upload

---
+++ Pounds_kilograms.py
@@ -0,0 +1,3 @@
+x = int(input("Please enter weight in pounds....."))
+print("Now converting pounds to kilograms.....")
+y = print(f"{x / 2.204 = } ")
---
+++ kilometres_miles.py
@@ -0,0 +1,3 @@
+x = int(input ("How many kilometres are there?"))
+print("Now converting kilometres to miles")
+print(f"{x * 0.6214 =} miles")
